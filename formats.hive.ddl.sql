cd7e06081ece122414df40e7e665bc83c2c85e1e
Jeff L <jeff@svds.com>
2017-06-01 19:31:31 -0700

    added SMB join tables

--- formats.hive.ddl.sql
+++ formats.hive.ddl.sql
@@ -65,3 +65,65 @@ CREATE EXTERNAL TABLE order_products__prior_avro (
 STORED AS AVRO
 LOCATION '/user/cloudera/instacart/order_products__prior_avro/'
 ;
+
+SET hive.execution.engine = spark;
+
+ANALYZE TABLE products_parquet COMPUTE STATISTICS;
+ANALYZE TABLE orders_parquet COMPUTE STATISTICS;
+ANALYZE TABLE order_products__prior_parquet COMPUTE STATISTICS;
+
+ANALYZE TABLE products_avro COMPUTE STATISTICS;
+ANALYZE TABLE orders_avro COMPUTE STATISTICS;
+ANALYZE TABLE order_products__prior_avro COMPUTE STATISTICS;
+
+ANALYZE TABLE products COMPUTE STATISTICS;
+ANALYZE TABLE orders COMPUTE STATISTICS;
+ANALYZE TABLE order_products__prior COMPUTE STATISTICS;
+
+CREATE EXTERNAL TABLE order_products__prior_smb (
+  order_id          INT,
+  product_id        INT,
+  add_to_cart_order INT,
+  reordered         TINYINT
+)
+PARTITIONED BY (year INT)
+CLUSTERED BY (order_id) SORTED BY (order_id ASC) INTO 2 BUCKETS
+STORED AS PARQUET
+LOCATION '/user/cloudera/instacart/order_products__prior_smb/'
+;
+
+CREATE EXTERNAL TABLE orders_smb (
+  order_id               INT    ,
+  user_id                INT    ,
+  eval_set               STRING ,
+  order_number           INT    ,
+  order_dow              TINYINT,
+  order_hour_of_day      TINYINT,
+  days_since_prior_order DOUBLE
+)
+PARTITIONED BY (year INT)
+CLUSTERED BY (order_id) SORTED by (order_id ASC) INTO 2 BUCKETS
+STORED AS PARQUET
+LOCATION '/user/cloudera/instacart/orders_smb/'
+;
+
+SET hive.enforce.bucketing = true;
+
+ INSERT INTO TABLE orders_smb PARTITION (year = 2017)
+ SELECT *
+   FROM orders_parquet
+CLUSTER BY order_id
+;
+
+SET hive.execution.engine = mr;
+
+ INSERT INTO TABLE order_products__prior_smb PARTITION (year = 2017)
+ SELECT *
+   FROM order_products__prior_parquet
+CLUSTER BY order_id
+;
+
+SET hive.execution.engine = spark;
+
+ANALYZE TABLE orders_smb PARTITION (year = 2017) COMPUTE STATISTICS;
+ANALYZE TABLE order_products__prior_smb PARTITION (year = 2017) COMPUTE STATISTICS;
